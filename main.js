905beddf9bb70ba5512befb154753a83f261719a
Vansh Mohan <69002944+vanshman@users.noreply.github.com>
2021-06-03 17:18:03 +0530

    nice
    nice

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function addUser(){
     var username = document.getElementById("input").value;
     
-    localStorage.setItem("username", username);
+    localStorage.setItem("user_name", username);
     window.location = "kwitter_room.html";
 }
