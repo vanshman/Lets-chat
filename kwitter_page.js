3943c92df8af17abf80af03b899a71da2e4ad022
Vansh Mohan <69002944+vanshman@users.noreply.github.com>
2021-06-14 16:49:51 +0530

    nice
    nice

--- kwitter_page.js
+++ kwitter_page.js
@@ -17,9 +17,9 @@ var room_name = localStorage.getItem("Room");
 function send(){
     var msg = document.getElementById("msg").value;
     firebase.database().ref(room_name).push({
-        "name": user_name,
-        "msg": msg,
-        "likes": 0
+        name: user_name,
+        msg: msg,
+        likes: 0
     });
 }
 function logout(){
@@ -43,18 +43,32 @@ function logout(){
     window.location = "index.html";
 }
 function getData() {
-    firebase.database().ref("/").on('value', function(snapshot) {
-          document.getElementById("output").innerHTML = "";
-          snapshot.forEach(function(childSnapshot) {
-                childKey  = childSnapshot.key;
-                Room_names = childKey;
-                //Start code
-                console.log("Room Name -" + Room_names);
-                var row = "<div class='room_name' id='" + Room_names + " onclick='redirectToRoom(this.id)'># " + Room_names + "</div><hr>";
-                document.getElementById("output").innerHTML += row;
+    firebase.database().ref("/" + room_name).on('value', function(snapshot) {
+        document.getElementById("output").innerHTML = ""; 
+        snapshot.forEach(function(childSnapshot) { 
+            childKey = childSnapshot.key; childData = childSnapshot.val(); 
+            if(childKey != "purpose") {
+                firebase_message_id = childKey;
+                message_data = childData;
 
-                //End code
+                message = message_data["msg"];
+                likes = message_data["likes"];
+                name = message_data["name"];
+                message_html = "<p class='message_h4'>" + message + "</p>";
+                name_html = "<h4>" + name + "</h4>";
+                like_html = "<button class='btn btn-primary' id='" + firebase_message_id + "' value=" + likes + " onclick='updateLike(this.id)'><span class='glyphicon glyphicon-thumbs-up'>Like: " + likes + "</span></button><b><hr>";
+                row = name_html + message_html + like_html;
+                document.getElementById("output").innerHTML += row;
+            }
           });
+    });
+}
+function updateLike(message_id){
+    likes = document.getElementById(message_id).value;
+    updated_likes = Number(likes) + 1;
+
+    firebase.database().ref(room_name).child(message_id).update({
+        likes: updated_likes
     });
 }
 getData();
